8ad31bf476a96eee0e8bfa188a608660316c1f4b
zacke <zackepe@gmail.com>
2022-08-28 18:38:39 +0300

    tehtävä 2.3

---
+++ task2.3.py
@@ -0,0 +1,7 @@
+base = input("Anna suorakulmion kanta: ")
+height = input("Anna suorakulmion korkeus: ")
+
+area = float(base) * float(height)
+peri = float(base) * 2 + float(height) * 2
+
+print(f"Suorakulmion pinta-ala on {area} ja piiri on {peri}")
