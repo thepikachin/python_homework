4eb7dc089d9f4ef25bb3843244b003e491b35c84
zacke <zackepe@gmail.com>
2022-08-31 18:21:20 +0300

    tehtävä 2.6

---
+++ task2.6.py
@@ -0,0 +1,11 @@
+import random
+
+alpha1 = random.randint(0, 9)
+alpha2 = random.randint(0, 9)
+alpha3 = random.randint(0, 9)
+beta1 = random.randint(1, 6)
+beta2 = random.randint(1, 6)
+beta3 = random.randint(1, 6)
+beta4 = random.randint(1, 6)
+
+print (f"Numerokoodi 1: {alpha1} {alpha2} {alpha3}\nNumerokoodi 2: {beta1} {beta2} {beta3} {beta4}")
