f603aaf88b64a4a23b83bdfc0a1f5b01e315f890
zacke <zackepe@gmail.com>
2022-10-09 15:38:20 +0300

    tehtävä 6.4

---
+++ week3/exercise6.4.py
@@ -0,0 +1,9 @@
+def integersum(integers):
+    sum = 0
+    for i in integers:
+        sum = sum + i
+    return sum
+
+integers = [1, 1, 2, 3, 5, 8, 11]
+sum = integersum(integers)
+print(f"Listan kokonaislukujen summa on {sum}.")
