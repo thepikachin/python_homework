377cade439d23b1e0d12346bb47d1063b979c38f
zacke <zackepe@gmail.com>
2022-10-09 15:48:38 +0300

    tehtävä 6.5

--- week3/exercise6.5.py
+++ week3/exercise6.5.py
@@ -1,7 +1,11 @@
 def nonodd(integerlist):
+    nonoddlist = []
     for i in integerlist:
         if i % 2 == 0:
-            integernonodd.append(i)
-    return integernonodd
+            nonoddlist.append(i)
+    return nonoddlist
 
-integerlist = [1, 1, 2, 3, 5, 8, 13]
+integerlist = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
+nonoddlist = nonodd(integerlist)
+print(f"Muokkaamaton lista kokonaislukuja näyttää tältä:\n{integerlist}")
+print(f"Parittomat luvut karsittuna lista näyttää tältä:\n{nonoddlist}")
