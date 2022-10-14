8c39247d56e266f0dec6a06e7811a1abcc6523ba
zacke <zackepe@gmail.com>
2022-10-14 20:39:05 +0300

    tehtävä 7.2

--- week4/exercise7.2.py
+++ week4/exercise7.2.py
@@ -2,11 +2,11 @@ names = set()
 name = "null"
 while name != "":
     name = input("Anna nimi: ")
-    if name in names:
-        print("Aiemmin syötetty nimi")
-    else:
-        print("Uusi nimi")
     if name != "":
+        if name in names:
+            print("Aiemmin syötetty nimi")
+        else:
+            print("Uusi nimi")
         names.add(name)
 print(f"Tässä antamasi nimet:")
 for name in names:
---
+++ week4/exercise7.3.py
@@ -0,0 +1,7 @@
+airports = {}
+option = ""
+while option != "lopeta":
+    option = str(input("Jos haluat syöttää uuden lentokentän, kirjoita 'syötä' ja paina Enter.\n"
+                       "Jos haluat hakea olemassa olevan lentokentän tiedot, kirjoita 'hae' ja paina Enter.\n"
+                       "Jos haluat lopettaa, kirjoita 'lopeta' ja paina Enter.\n"))
+    while
