378ea73e0f39f6c33d06fb32de0d45f3b571865a
zacke <zackepe@gmail.com>
2022-10-15 17:14:15 +0300

    tehtävä 7.3

--- week4/exercise7.3.py
+++ week4/exercise7.3.py
@@ -1,7 +1,19 @@
 airports = {}
-option = ""
+option = airport = icao = ""
 while option != "lopeta":
     option = str(input("Jos haluat syöttää uuden lentokentän, kirjoita 'syötä' ja paina Enter.\n"
                        "Jos haluat hakea olemassa olevan lentokentän tiedot, kirjoita 'hae' ja paina Enter.\n"
                        "Jos haluat lopettaa, kirjoita 'lopeta' ja paina Enter.\n"))
-    while
+    if option != "lopeta":
+        if option == "syötä":
+            icao = str(input("Anna lentoaseman ICAO-koodi: "))
+            airport = str(input("Anna lentoaseman nimi: "))
+            airports[icao] = airport
+        elif option == "hae":
+            search = str(input("Anna haluamasi lentoaseman ICAO-koodi: "))
+            if search in airports:
+                print(f"Lentokenttä {search} on {airports[search]}")
+            else:
+                print(f"Lentokenttää {search} ei löydy tietokannasta.")
+        else:
+            print("Virheellinen syöte")
